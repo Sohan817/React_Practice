5b865f09cea7739fbc57004bc6338c548511099b
Sohan817 <hsohan509@gmail.com>
2023-08-23 20:10:43 +0600

    Adding Word count, Character Count and Reading Time to TextUtils

--- Chapter 1/textutils/src/components/TextFrom.js
+++ Chapter 1/textutils/src/components/TextFrom.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function TextFrom(props) {
-  const [text, setText] = useState("Enter Text Here");
+  const [text, setText] = useState(" ");
   const handleOnChange = (event) => {
     //console.log("Change click");
     setText(event.target.value);
@@ -11,23 +11,42 @@ export default function TextFrom(props) {
     var newText = text.toUpperCase();
     setText(newText);
   };
+  const handleLowClick = () => {
+    //console.log("Uppercase button click", text);
+    var newText = text.toLowerCase();
+    setText(newText);
+  };
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <label htmlFor="myBox" className="form-label"></label>
-        <textarea
-          className="form-control"
-          value={text}
-          id="myBox"
-          onChange={handleOnChange}
-          // @ts-ignore
-          rows="8"
-        ></textarea>
+    <>
+      <div>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <label htmlFor="myBox" className="form-label"></label>
+          <textarea
+            className="form-control"
+            value={text}
+            id="myBox"
+            onChange={handleOnChange}
+            // @ts-ignore
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+          Convert to Lowercase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to Uppercase
-      </button>
-    </div>
+      <div className="container my-3">
+        <h2>Your Text Summary</h2>
+        <p>
+          {text.split(" ").length} words, {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minuts to read the Paragraph</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
