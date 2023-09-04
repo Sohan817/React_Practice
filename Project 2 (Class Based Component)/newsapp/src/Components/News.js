1a56386ac271708aaf326c8f8c41516fd4ff0a87
Sohan817 <hsohan509@gmail.com>
2023-09-04 18:15:15 +0600

    Fetching API key from news API

--- Project 2 (Class Based Component)/newsapp/src/Components/News.js
+++ Project 2 (Class Based Component)/newsapp/src/Components/News.js
@@ -4,14 +4,19 @@ NewsItem;
 export default class News extends Component {
   render() {
     return (
-      <div>
-        This is a news components
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className="container my-3">
+        <h1>NewsMonkey-Top Headlines</h1>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="My title" description="medesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="My title" description="medesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="My title" description="medesc" />
+          </div>
+        </div>
       </div>
     );
   }
--- Project 2 (Class Based Component)/newsapp/src/Components/NewsItem.js
+++ Project 2 (Class Based Component)/newsapp/src/Components/NewsItem.js
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    return <div>This is news item component</div>;
+    let { title, description } = this.props;
+    return (
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://sportshub.cbsistatic.com/i/r/2023/09/03/9f43b5c6-87c2-4c65-ac21-48687cfcbb2c/thumbnail/1200x675/a6ba3e2e53097d1e98f280eb781d2b8d/graphic-w1movers.png"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsdetail" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
