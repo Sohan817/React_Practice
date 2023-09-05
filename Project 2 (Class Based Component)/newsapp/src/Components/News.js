3742803d2d01c6be71cbe8c8f9436f413ef33812
Sohan817 <hsohan509@gmail.com>
2023-09-05 17:10:55 +0600

    Adding previous and next button to newsitem

--- Project 2 (Class Based Component)/newsapp/src/Components/News.js
+++ Project 2 (Class Based Component)/newsapp/src/Components/News.js
@@ -8,20 +8,53 @@ export default class News extends Component {
     this.state = {
       articles: [],
       loading: true,
+      page: 1,
     };
   }
   async componentDidMount() {
     //get url
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=fcf7501f86ec459bb99f51f6ccb8a492";
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=fcf7501f86ec459bb99f51f6ccb8a492&pageSize=18";
+    //fetch the api
+    let data = await fetch(url);
+    //Convert to json
+    let parseData = await data.json();
+    //pass data to state article
+    this.setState({
+      articles: parseData.articles,
+      totalArticles: parseData.totalResults,
+    });
+  }
+  handlePreviousClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=fcf7501f86ec459bb99f51f6ccb8a492&page=${
+      this.state.page - 1
+    } &pageSize=18`;
     //fetch the api
     let data = await fetch(url);
     //Convert to json
     let parseData = await data.json();
     console.log(parseData);
     //pass data to state article
-    this.setState({ articles: parseData.articles });
-  }
+    this.setState({ page: this.state.page - 1, articles: parseData.articles });
+  };
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalArticles / 18)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=fcf7501f86ec459bb99f51f6ccb8a492&page=${
+        this.state.page + 1
+      }&pageSize=18`;
+      //fetch the api
+      let data = await fetch(url);
+      //Convert to json
+      let parseData = await data.json();
+      console.log(parseData);
+      //pass data to state article
+      this.setState({
+        page: this.state.page + 1,
+        articles: parseData.articles,
+      });
+    }
+  };
   render() {
     return (
       <div className="container my-3">
@@ -49,6 +82,23 @@ export default class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePreviousClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
