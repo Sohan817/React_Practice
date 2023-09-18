56d2e050803fed3f28046173cb990607c9037da4
Sohan817 <hsohan509@gmail.com>
2023-09-18 16:24:45 +0600

    Setting up a React Application for Redux

---
+++ reduxapp/src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Shop() {
+  return (
+    <>
+      <h1>Buy Addidas Shows 50 USD</h1>
+      <div className="container">
+        <button className="btn btn-primary mx-2">-</button>
+        Add to cart
+        <button className="btn btn-primary mx-2">+</button>
+      </div>
+    </>
+  );
+}
